18e1725e0a6f119af4b3da032e059a2014b34cf9
mathieu <mathieu@debian-BULLSEYE-live-builder-AMD64>
2022-01-23 10:55:10 -0500

    retour sous debian

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,7 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar HDMI-1 2>&1 | tee -a /tmp/polybar1.log & disown
+sleep 1 &&
 polybar eDP-1 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
