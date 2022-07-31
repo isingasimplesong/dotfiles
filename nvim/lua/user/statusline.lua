39ed5966826aeec25585368d9d5d83fcb1402f2f
Mathieu Rousseau <mathieu@400iso.net>
2022-07-31 12:50:00 -0400

    <blank>

--- nvim/lua/user/statusline.lua
+++ nvim/lua/user/statusline.lua
@@ -1,72 +1,74 @@
 -- status line
+
+-- Airline
 vim.cmd([[let g:airline_theme='base16_nord']])
 vim.cmd([[let g:airline#extensions#tabline#enabled = 1]])
 
---
--- require('lualine').setup {
---   options = {
---     icons_enabled = true,
---     theme = 'auto',
---     -- theme = 'material',
---     component_separators = { left = '', right = ''},
---     section_separators = { left = '', right = ''},
---     disabled_filetypes = {
---       statusline = {},
---       winbar = {},
---     },
---     ignore_focus = {},
---     always_divide_middle = true,
---     globalstatus = false,
---     refresh = {
---       statusline = 1000,
---       tabline = 1000,
---       winbar = 1000,
---     }
---   },
---   sections = {
---     lualine_a = {'mode'},
---     lualine_b = {'branch', 'diff', 'diagnostics'},
---     lualine_c = {'filename'},
---     lualine_x = {'location'},
---     lualine_y = {'encoding', 'fileformat', 'filetype'},
---     lualine_z = {'progress'}
---   },
---   inactive_sections = {
---     lualine_a = {},
---     lualine_b = {},
---     lualine_c = {'filename'},
---     lualine_x = {'location'},
---     lualine_y = {},
---     lualine_z = {}
---   },
---   tabline = {
---       lualine_a = {
---       { 
---         'buffers',
---         show_filename_only = false,
---         show_modified_status = true,
---         mode = 0, -- 0 : buffer name
---                   -- 1 : buffer index
---                   -- 2 : name + index
---                   -- 3 : number
---                   -- 4 : name + number
---         filetype_names = { 
---           TelescopePrompt = 'Telescope',
---           dashboard = 'Dashboard',
---           packer = 'Packer',
---           fzf = 'FZF',
---           alpha = 'Alpha'
---         }
---
---       }
---     },
---       lualine_b = {},
---       lualine_c = {},
---       lualine_x = {},
---       lualine_y = {},
---       lualine_z = {'hostname'}
---   },
---   winbar = {},
---   inactive_winbar = {},
---   extensions = {}
--- }
+--[[ -- Lualine (commenter une des deux config) 
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    -- theme = 'material',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {'encoding', 'fileformat', 'filetype'},
+    lualine_z = {'progress'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {
+      lualine_a = {
+      { 
+        'buffers',
+        show_filename_only = false,
+        show_modified_status = true,
+        mode = 0, -- 0 : buffer name
+                  -- 1 : buffer index
+                  -- 2 : name + index
+                  -- 3 : number
+                  -- 4 : name + number
+        filetype_names = { 
+          TelescopePrompt = 'Telescope',
+          dashboard = 'Dashboard',
+          packer = 'Packer',
+          fzf = 'FZF',
+          alpha = 'Alpha'
+        }
+
+      }
+    },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {'hostname'}
+  },
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+} ]]
