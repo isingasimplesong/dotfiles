9d838b105b8a9f7b7d5cd1411664959031d24669
Mathieu Rousseau <mathieu@400iso.net>
2023-06-06 23:02:37 -0400

    cleaning

--- qtile/config.py
+++ qtile/config.py
@@ -92,6 +92,7 @@ keys = [
     Key([mod], "s", lazy.group["scratchpad"].dropdown_toggle("term")),
     Key([mod], "g", lazy.group["scratchpad"].dropdown_toggle("gpt")),
     Key([mod], "b", lazy.group["scratchpad"].dropdown_toggle("bitwarden")),
+    Key([mod], "y", lazy.group["scratchpad"].dropdown_toggle("yt-music")),
 ]
 scratchpads = [
     ScratchPad("scratchpad", [
@@ -101,6 +102,8 @@ scratchpads = [
                  x=0.15, y=0.05, height=0.85, width=0.7, opacity=1,),
         DropDown("bitwarden", "bitwarden-desktop", x=0.15, y=0.05,
                  height=0.85, width=0.7, opacity=1, on_focus_lost_hide=True),
+        DropDown("yt-music", "youtube-music", x=0.15, y=0.05, height=0.85,
+                 width=0.7, opacity=1, on_focus_lost_hide=False),
     ]),
 ]
 
