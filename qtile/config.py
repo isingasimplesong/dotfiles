e509e6532274034bbfc8f61dcfc6c8e41eec55d1
Mathieu Rousseau <mathieu@400iso.net>
2023-06-08 08:17:40 -0400

    cleaning

--- qtile/config.py
+++ qtile/config.py
@@ -38,10 +38,8 @@ cursor_warp = False
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
-# string for java UI programs
+# lie for some java UI programs
 wmname = "LG3D"
