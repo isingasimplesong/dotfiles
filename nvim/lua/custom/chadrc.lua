4ff95e57854aa933a4fbccb3512972d6c03f580d
mathieu <mathieu@2027a.net>
2023-09-16 15:07:53 -0400

    custom theme config

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,6 +1,24 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'onenord' }
+M.ui = {
+  theme_toggle = { "onenord", "one_light" },
+  theme = 'onenord',
+  statusline = {
+    theme = "default",
+    separator_style = "arrow",
+  },
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      [[                               __                ]],
+    	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+	    [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+	    [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+	    [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+	    [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+    },
+  }
+}
 
 return M
