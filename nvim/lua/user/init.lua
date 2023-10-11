bb7b735ef36942890016c289c00878c7729d2716
mathieu <mathieu@2027a.net>
2023-10-10 22:09:48 -0400

    split config

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,3 +1,15 @@
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
 require("user.options")
 require("user.keymap")
 require("user.lazy")
+require("user.telescope")
+require("user.treesitter")
+require("user.lsp")
+require("user.cmp")
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
