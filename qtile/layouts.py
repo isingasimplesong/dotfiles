6a23f19c0a5caf57c229a1f9d49068f2e7754341
mathieu <mathieu@2027a.net>
2023-10-31 13:45:00 -0400

    rationalize hidden border colors

--- qtile/layouts.py
+++ qtile/layouts.py
@@ -2,14 +2,14 @@ from libqtile import layout
 from colors import colors
 
 layouts = [
-    layout.MonadTall(border_width=0, ratio=0.6, margin=8,),
+    layout.MonadTall(border_width=0, ratio=0.6, margin=8, border_focus=colors[14], border_normal=colors[0],),
     layout.MonadWide(border_width=0, ratio=0.6, margin=8,),
     layout.RatioTile(border_width=0, margin=8,),
     layout.Columns(
         border_focus_stack=[colors[11], colors[12]],
-        border_width=0,
+        border_width=2,
         margin=8,
         ),
-    layout.Floating(border_width=0,),
+    layout.Floating(border_width=0, border_focus=colors[14], border_normal=colors[0],),
     layout.Max(border_width=0, margin=8,),
 ]
