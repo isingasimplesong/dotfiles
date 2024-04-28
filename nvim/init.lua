2b1e9143ca042eb1ae0d36f5e92ab6bd28c3a344
mathieu <mathieu@2027a.net>
2024-04-28 02:44:13 -0400

    better colorscheme management

--- nvim/init.lua
+++ nvim/init.lua
@@ -709,8 +709,6 @@ require('lazy').setup({
     end,
   },
 
-  { 'navarasu/onedark.nvim' },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
