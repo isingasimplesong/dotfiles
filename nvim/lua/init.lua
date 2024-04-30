fb11cc69483c13f912b00fa6ceed662dcb49fcee
mathieu <mathieu@2027a.net>
2024-04-30 13:30:06 -0400

    cleanning

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -3,7 +3,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
+lsp
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
