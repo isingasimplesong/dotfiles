d1904a0c2632478a855dc937f5dc7dd4dfe11a37
mathieu <mathieu@2027a.net>
2024-04-30 13:29:10 -0400

    let lualine auto adjust it's theme

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'onenord',
+      -- theme = 'onenord',
       component_separators = '',
       section_separators = { left = '', right = '' },
     },
