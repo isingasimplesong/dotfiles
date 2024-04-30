eca463bb62204d75cbb53c8f67182c61da76950f
mathieu <mathieu@2027a.net>
2024-04-29 22:21:53 -0400

    modularisation

---
+++ nvim/lua/autocommands.lua
@@ -0,0 +1,14 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- vim: ts=4 sts=4 sw=4 et
--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,7 +1,10 @@
 -- from kickstart.nvim - https://github.com/nvim-lua/kickstart.nvim
 
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 require 'options'
@@ -10,4 +13,5 @@ require 'autocommands'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
 
+-- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=4 sts=4 sw=4 et
