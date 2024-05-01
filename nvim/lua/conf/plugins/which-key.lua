dee73f76ace7f267879b5376a1d8854c13940f74
mathieu <mathieu@2027a.net>
2024-05-01 10:40:23 -0400

    formatting

--- nvim/lua/conf/plugins/which-key.lua
+++ nvim/lua/conf/plugins/which-key.lua
@@ -12,6 +12,8 @@ return { -- Useful plugin to show you pending keybinds.
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      -- ['<leader>['] = { name = 'Previous', _ = 'which_key_ignore' },
+      -- ['<leader>]'] = { name = 'Next', _ = 'which_key_ignore' },
       -- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
     }
     -- visual mode
