562c334e5342c6a1282de06af028aaf53e807c28
mathieu <mathieu@2027a.net>
2024-05-01 00:52:18 -0400

    refactor

--- nvim/lua/conf/plugins/treejs.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'Wansmer/treesj',
-  keys = { '<space>m', '<space>j', '<space>k' },
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  config = function()
-    require('treesj').setup {--[[ your config ]]
-    }
-  end,
-}
