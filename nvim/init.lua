416d6719d8c41fbdfd47d4b5ae9454600a604471
mathieu <mathieu@2027a.net>
2024-05-01 00:41:59 -0400

    refactor

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,7 @@
 -- from kickstart.nvim - https://github.com/nvim-lua/kickstart.nvim
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
-
 require 'conf.core'
 
 -- vim: ts=4 sts=4 sw=4 et
