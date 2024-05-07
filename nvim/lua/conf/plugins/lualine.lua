085fe95f80ce97930fd02a42dc5c0f40bbc0d7cd
mathieu <mathieu@2027a.net>
2024-05-07 18:05:21 -0400

    configuring noice & lualine for showing @recording macro

--- nvim/lua/conf/plugins/lualine.lua
+++ nvim/lua/conf/plugins/lualine.lua
@@ -9,4 +9,17 @@ return {
       section_separators = { left = '', right = '' },
     },
   },
+  config = function()
+    require('lualine').setup {
+      sections = {
+        lualine_x = {
+          {
+            require('noice').api.statusline.mode.get,
+            cond = require('noice').api.statusline.mode.has,
+            color = { fg = '#ff9e64' },
+          },
+        },
+      },
+    }
+  end,
 }
