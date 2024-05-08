6294a2b48ec22134309db495bcf503676b7491ce
mathieu <mathieu@2027a.net>
2024-05-07 20:23:18 -0400

    restore lualine config

--- nvim/lua/conf/plugins/lualine.lua
+++ nvim/lua/conf/plugins/lualine.lua
@@ -1,16 +1,14 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = {
-      icons_enabled = true,
-      -- theme = 'onenord',
-      component_separators = '',
-      section_separators = { left = '', right = '' },
-    },
-  },
   config = function()
     require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        -- theme = 'onenord',
+        component_separators = '',
+        section_separators = { left = '', right = '' },
+      },
       sections = {
         lualine_x = {
           {
