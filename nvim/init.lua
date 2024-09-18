96d8b4535ca91179436d9f7c0113a85e43ab82d7
mathieu <mathieu@2027a.net>
2024-09-17 22:09:26 -0400

    better hint for Oil in which-keys

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,7 +94,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Oil.nvim
-vim.keymap.set('n', '<leader>e', '<cmd>Oil<CR>', { desc = 'Op[e]n Oil' })
+vim.keymap.set('n', '<leader>e', '<cmd>Oil<CR>', { desc = '[E]xplorateur' })
 
 -- Close buffer
 vim.keymap.set('n', '<leader>c', '<cmd>bdelete<CR>', { desc = '[C]lose Buffer' })
@@ -791,8 +791,6 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'onenord'
 
       -- You can configure highlights by doing something like:
