8c5f7571aadbc3822b99ff1ec9802b63bb6a953a
mathieu <mathieu@2027a.net>
2024-09-18 09:14:15 -0400

    split nvim config

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,20 @@
+return {
+  { -- You can easily change to a different colorscheme.
+    'rmehri01/onenord.nvim',
+    -- 'ellisonleao/gruvbox.nvim',
+    -- 'navarasu/onedark.nvim',
+    -- 'rose-pine/neovim',
+    -- 'catppuccin/nvim',
+    -- 'folke/tokyonight.nvim',
+    -- 'Mofiqul/dracula.nvim',
+    -- 'sainnhe/sonokai',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      vim.cmd.colorscheme 'onenord'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
--- nvim/lua/plugins/colorschemes.lua.bak
+++
@@ -1,9 +0,0 @@
-return {
-  'ellisonleao/gruvbox.nvim',
-  'navarasu/onedark.nvim',
-  'rose-pine/neovim',
-  'catppuccin/nvim',
-  'folke/tokyonight.nvim',
-  'Mofiqul/dracula.nvim',
-  'sainnhe/sonokai',
-}
