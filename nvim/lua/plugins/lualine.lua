e84149eecd42b45c02171a9305cf43aa3f363659
mathieu <mathieu@2027a.net>
2024-09-18 23:48:31 -0400

    better lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,19 +5,51 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        -- theme = 'onenord',
+        theme = 'auto',
         component_separators = '',
-        section_separators = { left = '', right = '' },
-      },
-      sections = {
-        lualine_x = {
-          {
-            require('noice').api.statusline.mode.get,
-            cond = require('noice').api.statusline.mode.has,
-            color = { fg = '#ff9e64' },
-          },
+        -- component_separators = { left = '', right = '' },
+        section_separators = '',
+        -- section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
         },
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {
+        -- lualine_a = { 'buffers' },
+        -- lualine_b = { 'branch' },
+        -- lualine_c = { 'filename' },
+        -- lualine_x = {},
+        -- lualine_y = {},
+        -- lualine_z = { 'tabs' },
+      },
+      winbar = {},
+      inactive_winbar = {},
+      extensions = { 'man', 'oil' },
     }
   end,
 }
