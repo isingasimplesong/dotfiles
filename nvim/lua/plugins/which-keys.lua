95a908003ce194c0d22f477fda962636877aa658
mathieu <mathieu@2027a.net>
2024-09-20 21:03:28 -0400

    no icons in which-keys

--- nvim/lua/plugins/which-keys.lua
+++ nvim/lua/plugins/which-keys.lua
@@ -5,7 +5,8 @@ return {
     opts = {
       icons = {
         -- set icon mappings to true if you have a Nerd Font
-        mappings = vim.g.have_nerd_font,
+        mappings = false,
+        -- mappings = vim.g.have_nerd_font,
         -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
         -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
         keys = vim.g.have_nerd_font and {} or {
