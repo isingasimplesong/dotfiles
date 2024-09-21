a10caaebd019c62ed021500c5a59f1a707519414
mathieu <mathieu@2027a.net>
2024-09-21 00:44:29 -0400

    cleaning

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,27 +1,9 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    'rmehri01/onenord.nvim',
-    -- 'ellisonleao/gruvbox.nvim',
-    -- 'navarasu/onedark.nvim',
-    -- 'rose-pine/neovim',
-    -- 'catppuccin/nvim',
-    -- 'folke/tokyonight.nvim',
-    -- 'Mofiqul/dracula.nvim',
-    -- 'sainnhe/sonokai',
-    opts = {
-      disable = {
-        float_background = true,
-      },
-      enable = {
-        cursorline = true,
-      },
-    },
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      vim.cmd.colorscheme 'onenord'
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  'ellisonleao/gruvbox.nvim',
+  'navarasu/onedark.nvim',
+  'rose-pine/neovim',
+  'catppuccin/nvim',
+  'folke/tokyonight.nvim',
+  'Mofiqul/dracula.nvim',
+  'sainnhe/sonokai',
 }
