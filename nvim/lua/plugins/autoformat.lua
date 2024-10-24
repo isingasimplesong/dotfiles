375ce2048760da704b15d56a2ce6e568e21f9793
Mathieu Rousseau <mathieu@2027a.net>
2024-10-23 22:02:02 -0400

    add bash formater

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -33,6 +33,8 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        sh = { 'shfmt' },
+        bash = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         --
