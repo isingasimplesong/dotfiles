7970f52cb1912a1ff09b4ed7b0756909f6be4f80
Mathieu Rousseau <mathieu@2027a.net>
2025-05-05 23:09:26 -0400

    cleaning

--- nvim/lua/plugins/cmp-autocompletion.lua
+++ nvim/lua/plugins/cmp-autocompletion.lua
@@ -50,8 +50,6 @@ return {
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
           ['<C-n>'] = cmp.mapping.select_next_item(),
